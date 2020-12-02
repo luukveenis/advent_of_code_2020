29bca21e9d0d1ba77d7f1f2da4ae1e0bd18b0231
Luuk Veenis <luuk@juul.com>
2020-12-01 20:01:22 -0800

    Rename input file
    I thought there were going to be two separate inputs for the different
    parts of the problem, but since there's only one the 1_1 wasn't needed.

--- src/main/java/aoc/Runner.java
+++ src/main/java/aoc/Runner.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Runner {
     public static void main(String[] args) throws IOException {
-        var day1input = new File(Runner.class.getClassLoader().getResource("day1_1.txt").getFile());
+        var day1input = new File(Runner.class.getClassLoader().getResource("day1.txt").getFile());
         var day1 = new Day1(day1input);
 
         System.out.println("Day 1, part 1: " + day1.part1());
