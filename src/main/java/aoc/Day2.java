e1b32bc2e7f979cca3152907e10844e34ae15644
Luuk Veenis <luuk@juul.com>
2020-12-02 11:14:54 -0800

    Set up boilerplate for Day2 solution
    This adds the class that will contain the solution and associated test
    class, but doesn't implement anything yet.

---
+++ src/main/java/aoc/Day2.java
@@ -0,0 +1,15 @@
+package aoc;
+
+import java.io.File;
+
+public class Day2 {
+    private File input;
+
+    protected Day2(File input) {
+        this.input = input;
+    }
+
+    public int part1() {
+       return 2;
+    }
+}
---
+++ src/test/java/aoc/Day2Test.java
@@ -0,0 +1,28 @@
+package aoc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day2Test {
+    private Day2 day2;
+
+    @BeforeEach
+    public void setUp() {
+        var inputFile = new File(
+                getClass()
+                        .getClassLoader()
+                        .getResource("day2.txt")
+                        .getFile()
+        );
+        this.day2 = new Day2(inputFile);
+    }
+
+    @Test
+    public void part1_withTestInput_has2ValidPasswords() {
+        assertEquals(2, day2.part1(), "Solution was incorrect");
+    }
+}
