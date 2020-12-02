7fee688e0d3b3bef81d4aed0eb33c83a89b98abd
Luuk Veenis <luuk@juul.com>
2020-12-02 11:50:33 -0800

    Add Day 2, part 1 solution to runner output

--- src/main/java/aoc/Runner.java
+++ src/main/java/aoc/Runner.java
@@ -10,5 +10,11 @@ public class Runner {
 
         System.out.println("Day 1, part 1: " + day1.part1());
         System.out.println("Day 1, part 2: " + day1.part2());
+
+        var day2input = new File(Runner.class.getClassLoader().getResource("day2.txt").getFile());
+        var day2 = new Day2(day2input);
+
+        System.out.println("Day2, part 1: " + day2.part1());
+        // System.out.println("Day2, part 2: " + day2.part2());
     }
 }
