cea3311a5aed639afe0e259f0add75d8e2574696
Luuk Veenis <luuk@juul.com>
2020-12-02 11:41:09 -0800

    Implement solution for Day 2, part 1
    This adds a simple method to check if a given string is valid for the
    requirements stated in the problem. We then filter the stream of lines
    from the input file through this method to get all "valid" lines and
    return the total count.
    
    It's pretty obvious at this point there is going to be repeated code for
    both the solutions and the tests that should be cleaned up. The common
    boilerplate will be pulled out in a follow-up commit.

--- src/main/java/aoc/Day2.java
+++ src/main/java/aoc/Day2.java
@@ -1,8 +1,16 @@
 package aoc;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day2 {
+    private final Pattern PASSWORD_PATTERN = Pattern.compile(
+            "^(?<min>\\d)-(?<max>\\d) (?<char>\\w): (?<password>.*)$"
+    );
     private File input;
 
     protected Day2(File input) {
@@ -10,6 +18,30 @@ public class Day2 {
     }
 
     public int part1() {
-       return 2;
+       try (Stream<String> passwords = Files.lines(input.toPath())) {
+           return passwords
+                   .filter(this::isValid)
+                   .collect(Collectors.toList())
+                   .size();
+       } catch (IOException e) {
+           e.printStackTrace();
+           throw new RuntimeException("Input file does not exist");
+       }
+    }
+
+    private boolean isValid(String passwordLine) {
+        var matcher = PASSWORD_PATTERN.matcher(passwordLine);
+
+        if (matcher.find()) {
+            var min = Integer.parseInt(matcher.group("min"));
+            var max = Integer.parseInt(matcher.group("max"));
+            var character = matcher.group("char").charAt(0);
+            var password = matcher.group("password");
+            var occurrences = password.chars().filter(c -> c == character).count();
+
+            return occurrences >= min && occurrences <= max;
+        }
+
+        return false;
     }
 }
