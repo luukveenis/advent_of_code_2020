eca115354998f54b700a938951c68d294b624beb
Luuk Veenis <luuk@juul.com>
2020-12-13 14:40:21 -0800

    Use graphs to solve Day 7 part 1
    This is a very simplistic graph implementation, but it's a convenient
    way to represent the problem. We simply read the file, generate a graph
    of bag objects, and count all the bags from which we can find a path to
    the desired bag.
    
    This makes some assumptions about the input. Clearly the graph is
    directed, but it assumes there are no cycles or the program would loop
    infinitely. This seemed like a reasonable assumption since a cycle
    implies a bag contains itself, which is nonsense.

---
+++ ruby/day7/day7.rb
@@ -0,0 +1,69 @@
+require 'pry'
+
+# The "content" of a bag is essentially a weighted, directional edge
+# in the graph. It specifies the quantity of a specific colour bag that
+# are contained within another bag.
+class Content
+  attr_reader :bag, :count
+
+  def initialize(bag, count)
+    @bag = bag
+    @count = count
+  end
+end
+
+# A "bag" is a node in our BagGraph. It has a colour and a collection of
+# other bags it contains. The contents are represented as directed, weighted
+# edges using the Content class.
+class Bag
+  attr_reader :colour, :contents
+
+  def initialize(colour)
+    @colour = colour
+    @contents = []
+  end
+
+  def add_content(content)
+    contents << content
+  end
+
+  def contains?(colour)
+    contents.any? { |c| c.bag.colour == colour || c.bag.contains?(colour) }
+  end
+end
+
+# We can treat this problem as a graph and the solutions are simply traversals
+# of that graph.
+class BagGraph
+  attr_reader :bags
+
+  def initialize
+    @bags = []
+  end
+
+  def add(bag)
+    bags << bag
+  end
+
+  def find(colour)
+    bags.find { |bag| bag.colour == colour }
+  end
+
+  def find_or_create(colour)
+    find(colour) || Bag.new(colour).tap { |b| add(b) }
+  end
+end
+
+graph = BagGraph.new
+
+File.foreach(ARGV[0], chomp: true) do |line|
+  colour, contents = line.match(/(\w+ \w+) bags contain (.*)./).captures
+  bag = graph.find_or_create(colour)
+
+  contents.scan(/(\d+) (\w+ \w+) bags?/).each do |count, inner_colour|
+    inner_bag = graph.find_or_create(inner_colour)
+    bag.add_content(Content.new(inner_bag, count))
+  end
+end
+
+puts "Part 1: " + graph.bags.count { |b| b.contains?("shiny gold") }.to_s
